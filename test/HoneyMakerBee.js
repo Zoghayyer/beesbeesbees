6a85556b5983861a3802ad4f78d524f00ff4afe1
Ahmed Zoghayyer <azzoghayyer@gmail.com>
2020-05-24 01:02:57 -0700

    Remove comments

--- test/HoneyMakerBee.js
+++ test/HoneyMakerBee.js
@@ -6,8 +6,6 @@ describe('HoneyMakerBee class functionality', function() {
     honeyBee = new HoneyMakerBee();
   });
 
-  /*  Overwrite methods from superclass  */
-
   it('should have an age property that is set to `10`', function() {
     expect(honeyBee.age).to.equal(10);
   });
@@ -15,8 +13,6 @@ describe('HoneyMakerBee class functionality', function() {
   it('should have a job property that is set to `make honey`', function() {
     expect(honeyBee.job).to.equal('make honey')
   });
-
-  /*  Inherited from superclass  */
 
   it('should have a color property inherited from `bee` that is set to `yellow`', function() {
     expect(honeyBee.color).to.equal('yellow');
@@ -29,8 +25,6 @@ describe('HoneyMakerBee class functionality', function() {
   it('should have an eat method that is inherited from grub', function() {
     expect(honeyBee.eat).to.be.a('function');
   });
-
-  /*  New methods and properties  */
 
   it('should have a `honeyPot` property that is set to `0`', function() {
     expect(honeyBee.honeyPot).to.equal(0);
@@ -52,5 +46,4 @@ describe('HoneyMakerBee class functionality', function() {
     honeyBee.giveHoney();
     expect(honeyBee.honeyPot).to.equal(2);
   });
-
 });
